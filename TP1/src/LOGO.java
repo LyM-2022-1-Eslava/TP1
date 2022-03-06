d211dcddc54f088a1c1623ac74405a0306dc0c4d
Juan Andrés Eslava Tovar <88861582+juaneslava@users.noreply.github.com>
2022-03-06 12:43:26 -0500

    Javacc completado

--- TP1/src/LOGO.java
+++ TP1/src/LOGO.java
@@ -44,11 +44,11 @@ public class LOGO implements LOGOConstants {
     case REPEAT:
     case PENUP:
     case PENDOWN:
-      ins();
+    case 13:
+      tp1();
       jj_consume_token(13);
     {if (true) return 0;}
       break;
-    case 13:
       jj_consume_token(13);
     {if (true) return 1;}
       break;
@@ -58,6 +58,26 @@ public class LOGO implements LOGOConstants {
       throw new ParseException();
     }
     throw new Error("Missing return statement in function");
+  }
+
+  static final public void tp1() throws ParseException {
+    label_1:
+    while (true) {
+      switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
+      case FORWARD:
+      case RIGHT:
+      case LEFT:
+      case REPEAT:
+      case PENUP:
+      case PENDOWN:
+        ;
+        break;
+      default:
+        jj_la1[1] = jj_gen;
+        break label_1;
+      }
+      ins();
+    }
   }
 
   static final public void ins() throws ParseException {
@@ -71,7 +91,7 @@ public class LOGO implements LOGOConstants {
       jj_consume_token(REPEAT);
       jj_consume_token(CONSTANT);
       jj_consume_token(14);
-      label_1:
+      label_2:
       while (true) {
         ins();
         switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
@@ -84,8 +104,8 @@ public class LOGO implements LOGOConstants {
           ;
           break;
         default:
-          jj_la1[1] = jj_gen;
-          break label_1;
+          jj_la1[2] = jj_gen;
+          break label_2;
         }
       }
       jj_consume_token(15);
@@ -95,7 +115,7 @@ public class LOGO implements LOGOConstants {
       pen();
       break;
     default:
-      jj_la1[2] = jj_gen;
+      jj_la1[3] = jj_gen;
       jj_consume_token(-1);
       throw new ParseException();
     }
@@ -116,7 +136,7 @@ public class LOGO implements LOGOConstants {
       jj_consume_token(CONSTANT);
       break;
     default:
-      jj_la1[3] = jj_gen;
+      jj_la1[4] = jj_gen;
       jj_consume_token(-1);
       throw new ParseException();
     }
@@ -131,7 +151,7 @@ public class LOGO implements LOGOConstants {
       jj_consume_token(PENDOWN);
       break;
     default:
-      jj_la1[4] = jj_gen;
+      jj_la1[5] = jj_gen;
       jj_consume_token(-1);
       throw new ParseException();
     }
@@ -147,13 +167,13 @@ public class LOGO implements LOGOConstants {
   static public Token jj_nt;
   static private int jj_ntk;
   static private int jj_gen;
-  static final private int[] jj_la1 = new int[5];
+  static final private int[] jj_la1 = new int[6];
   static private int[] jj_la1_0;
   static {
       jj_la1_init_0();
    }
    private static void jj_la1_init_0() {
-      jj_la1_0 = new int[] {0x27e0,0x7e0,0x7e0,0xe0,0x600,};
+      jj_la1_0 = new int[] {0x27e0,0x7e0,0x7e0,0x7e0,0xe0,0x600,};
    }
 
   /** Constructor with InputStream. */
@@ -174,7 +194,7 @@ public class LOGO implements LOGOConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 5; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 6; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -188,7 +208,7 @@ public class LOGO implements LOGOConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 5; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 6; i++) jj_la1[i] = -1;
   }
 
   /** Constructor. */
@@ -205,7 +225,7 @@ public class LOGO implements LOGOConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 5; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 6; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -215,7 +235,7 @@ public class LOGO implements LOGOConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 5; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 6; i++) jj_la1[i] = -1;
   }
 
   /** Constructor with generated Token Manager. */
@@ -231,7 +251,7 @@ public class LOGO implements LOGOConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 5; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 6; i++) jj_la1[i] = -1;
   }
 
   /** Reinitialise. */
@@ -240,7 +260,7 @@ public class LOGO implements LOGOConstants {
     token = new Token();
     jj_ntk = -1;
     jj_gen = 0;
-    for (int i = 0; i < 5; i++) jj_la1[i] = -1;
+    for (int i = 0; i < 6; i++) jj_la1[i] = -1;
   }
 
   static private Token jj_consume_token(int kind) throws ParseException {
@@ -296,7 +316,7 @@ public class LOGO implements LOGOConstants {
       la1tokens[jj_kind] = true;
       jj_kind = -1;
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) {
       if (jj_la1[i] == jj_gen) {
         for (int j = 0; j < 32; j++) {
           if ((jj_la1_0[i] & (1<<j)) != 0) {
